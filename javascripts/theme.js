d42dc39eef6737397d33f137ea69dfb20637e4bb
s-andy <s-andy@56a16d12-d5e3-4c45-bbbe-4ff787308cc7>
2013-12-06 13:00:12 +0000

    Responsive and other fixes
    git-svn-id: http://svn.s-andy.com/red-andy@22 56a16d12-d5e3-4c45-bbbe-4ff787308cc7

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,4 +1,3 @@
-// FIXME run on head load?
 var viewportTag;
 var headTag = document.head || document.getElementsByTagName('HEAD')[0];
 var metaTags = headTag.getElementsByTagName('META');
@@ -14,3 +13,29 @@ if (typeof viewportTag == 'undefined') {
     viewportTag.content = 'width=device-width, user-scalable=no';
     headTag.appendChild(viewportTag);
 }
+
+if (document.observe) {
+    if (window.matchMedia && window.matchMedia('(max-device-width: 600px)').matches) {
+        document.observe('dom:loaded', function() {
+            var tabs = $$('div.tabs');
+            var mainMenu = $('main-menu');
+            mainMenu.addClassName('tabs');
+            var tabsButtons = new Element('DIV');
+            tabsButtons.addClassName('tabs-buttons');
+            tabsButtons.style.display = 'none';
+            var tabLeft = new Element('BUTTON');
+            tabLeft.addClassName('tab-left');
+            tabLeft.observe('click', function() { moveTabLeft(this); });
+            tabsButtons.appendChild(tabLeft);
+            var tabRight = new Element('BUTTON');
+            tabRight.addClassName('tab-right');
+            tabRight.observe('click', function() { moveTabRight(this); });
+            tabsButtons.appendChild(tabRight);
+            mainMenu.appendChild(tabsButtons);
+            if (tabs.length == 0) {
+                Event.observe(window, 'load', function() { displayTabsButtons(); });
+                Event.observe(window, 'resize', function() { displayTabsButtons(); });
+            }
+        });
+    }
+} // else TODO jQuery $(document).ready() + try for non-mobile as well
