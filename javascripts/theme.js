5572723ae35e3a83c740d0401be78491ed6ae504
s-andy <s-andy@56a16d12-d5e3-4c45-bbbe-4ff787308cc7>
2013-12-15 12:23:28 +0000

    Made footer stick to bottom (#1725), added sidebar toggler and other fixes for mobile
    git-svn-id: http://svn.s-andy.com/red-andy@24 56a16d12-d5e3-4c45-bbbe-4ff787308cc7

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -15,26 +15,54 @@ if (typeof viewportTag == 'undefined') {
 }
 
 if (document.observe) {
-    if (window.matchMedia && window.matchMedia('(max-device-width: 600px)').matches) {
+    if (document.viewport && (document.viewport.getWidth() <= 600)) {
+        var toggleSidebar = function(el) {
+            var sidebar = $('sidebar');
+            if (el.hasClassName('desc')) {
+                el.removeClassName('desc');
+                el.addClassName('asc');
+            } else {
+                el.removeClassName('asc');
+                el.addClassName('desc');
+            }
+            sidebar.childElements().each(function(child) {
+                if (child.id != 'sidebar-toggler') {
+                    child.toggle();
+                }
+            });
+        }
+
         document.observe('dom:loaded', function() {
-            var tabs = $$('div.tabs');
             var mainMenu = $('main-menu');
-            mainMenu.addClassName('tabs');
-            var tabsButtons = new Element('DIV');
-            tabsButtons.addClassName('tabs-buttons');
-            tabsButtons.style.display = 'none';
-            var tabLeft = new Element('BUTTON');
-            tabLeft.addClassName('tab-left');
-            tabLeft.observe('click', function() { moveTabLeft(this); });
-            tabsButtons.appendChild(tabLeft);
-            var tabRight = new Element('BUTTON');
-            tabRight.addClassName('tab-right');
-            tabRight.observe('click', function() { moveTabRight(this); });
-            tabsButtons.appendChild(tabRight);
-            mainMenu.appendChild(tabsButtons);
-            if (tabs.length == 0) {
-                Event.observe(window, 'load', function() { displayTabsButtons(); });
-                Event.observe(window, 'resize', function() { displayTabsButtons(); });
+            if (mainMenu) {
+                var tabs = $$('div.tabs');
+                mainMenu.addClassName('tabs');
+                var tabsButtons = new Element('DIV');
+                tabsButtons.addClassName('tabs-buttons');
+                tabsButtons.style.display = 'none';
+                var tabLeft = new Element('BUTTON');
+                tabLeft.addClassName('tab-left');
+                tabLeft.observe('click', function() { moveTabLeft(this); });
+                tabsButtons.appendChild(tabLeft);
+                var tabRight = new Element('BUTTON');
+                tabRight.addClassName('tab-right');
+                tabRight.observe('click', function() { moveTabRight(this); });
+                tabsButtons.appendChild(tabRight);
+                mainMenu.appendChild(tabsButtons);
+                if (tabs.length == 0) {
+                    Event.observe(window, 'load', function() { displayTabsButtons(); });
+                    Event.observe(window, 'resize', function() { displayTabsButtons(); });
+                }
+            }
+            var sidebar = $('sidebar');
+            if (sidebar) {
+                var sidebarToggler = new Element('A');
+                sidebarToggler.id = 'sidebar-toggler';
+                sidebarToggler.addClassName('sort');
+                sidebarToggler.addClassName('asc');
+                sidebarToggler.observe('click', function() { toggleSidebar(this); });
+                sidebar.appendChild(sidebarToggler);
+                toggleSidebar(sidebarToggler);
             }
         });
     }
